6fb3de397ae01c528cd647bfbe9be09adf4e5e14
Akrima Huzaifa Akhtar <akrimaamirka@gmail.com>
2025-09-15 02:54:44 +0500

    Refactor scripts to use snake_case and modularize logic
    - Renamed scripts to use snake_case for consistency and maintainability.
    - Refactored product and purchase history extraction scripts to modular functions, added pricing tier logic, and improved file output structure.
    - Integrated product data extraction with HTML table generation and purchase history workflow.

---
+++ balajiwireless/create_html_table.py
@@ -0,0 +1,59 @@
+import ast
+from datetime import date
+import pandas as pd
+
+
+def create_html_table(excel_file):
+    """
+    Read the Excel file with product data,
+    convert the 'Features' column to HTML tables,
+    and save the updated data to a new Excel file.
+    """
+    # Read the Excel file
+    df = pd.read_excel(excel_file)
+
+    # Display the first few rows
+    #print(df.head())
+
+    features_column = df['Features']
+    #print(features_column)
+
+    def features_to_html_table(features_str):
+        # Convert string to dictionary
+        features = ast.literal_eval(features_str)
+        table_style = (
+            'font-family:arial,sans-serif;'
+            'border-collapse:collapse;'
+            'width:100%;'
+        )
+        th_td_style = (
+            'border:1px solid #dddddd;'
+            'text-align:left;'
+            'padding:8px;'
+        )
+        html = f'<table style="{table_style}">\n'
+        html += f'  <tr><th style="{th_td_style}">Feature</th><th style="{th_td_style}">Value</th></tr>\n'
+        for i, (key, value) in enumerate(features.items()):
+            row_bg = 'background-color:#dddddd;' if i % 2 == 1 else ''
+            html += (
+                f'  <tr style="{row_bg}">'
+                f'<td style="{th_td_style}">{key}</td>'
+                f'<td style="{th_td_style}">{str(value).replace(chr(10), "<br>")}</td>'
+                f'</tr>\n'
+            )
+        html += '</table>'
+        return html
+
+    for index, row in features_column.items():
+        # Convert each features string to an HTML table
+        html_table = features_to_html_table(row)
+        # Save the HTML table to a new column in the DataFrame
+        df.at[index, 'Features_HTML'] = html_table
+
+    # Save the updated DataFrame to same Excel file
+    df.to_excel(excel_file, index=False)
+
+
+if __name__ == "__main__":
+    excel_file = f'extracted_products_{date.today()} - Copy.xlsx'
+    create_html_table(excel_file)
